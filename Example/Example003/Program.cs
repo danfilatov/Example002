88135ccb0416be9e0ec6d6c9fd3aa5e649e88c8a
Denis Filatov <gooden@mail.ru>
2023-02-22 21:28:06 +0300

    Example003_v3

--- Example/Example003/Program.cs
+++ Example/Example003/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
-double numberA = 12;
-double numberB = 5;
-double result = numberA / numberB;
-Console.WriteLine(result);
+//double numberA = 12;
+//double numberB = 5;
+//double result = numberA / numberB;
+//Console.WriteLine(result);
 
 // int numberA = 35;
 // int numberB = 53;
 // int result = numberA + numberB;
 // Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
