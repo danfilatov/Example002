892cf318e2621304a6d34eab146aa91bb4596d3c
Denis Filatov <gooden@mail.ru>
2023-02-22 21:48:23 +0300

    Example003_v4

--- Example/Example003/Program.cs
+++ Example/Example003/Program.cs
@@ -1,17 +1,30 @@
-﻿using System;
-//double numberA = 12;
+﻿//double numberA = 12;
 //double numberB = 5;
 //double result = numberA / numberB;
 //Console.WriteLine(result);
 
-// int numberA = 35;
-// int numberB = 53;
-// int result = numberA + numberB;
-// Console.WriteLine(result);
+//int numberA = 35;
+//int numberB = 53;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
 
-int numberA = new Random().Next(1,10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+//int numberA = new Random().Next(1,10);
+//Console.WriteLine(numberA);
+//int numberB = new Random().Next(1,10);
+//Console.WriteLine(numberB);
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "маша")
+{
+ Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
